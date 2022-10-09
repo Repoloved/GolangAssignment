59a63624ab0c3fb0346790daa6a15e178fd5d859
Agil Fajar Dwi Prasetyo <agilfajar33@gmail.com>
2022-10-09 08:06:48 +0700

    complete phase1

--- assignment2/assignment-basic-2-v2/main.go
+++ assignment2/assignment-basic-2-v2/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"math"
+	"strings"
 )
 
+// date time format layout
+const TextDate = "2 January 2006"
+
 func GetDayDifference(date string) int {
-	return 0 // TODO: replace this
+	splitmin := strings.Split(date, " - ")
+	splitspace := strings.Split(date, " ")
+	start_date := splitmin[0] + " " + splitspace[5]
+	end_date := splitmin[1]
+	date1, _ := time.Parse(TextDate, start_date)
+	date2, _ := time.Parse(TextDate, end_date)
+	days := int(math.Ceil(date2.Sub(date1).Hours() / 24))
+	return days
 }
 
 func GetSalary(rangeDay int, data [][]string) map[string]string {
